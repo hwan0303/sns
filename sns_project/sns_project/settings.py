05bf4bee14fd4f5cd53890912ec359222f94d839
hwan0303 <asok369258@naver.com>
2021-04-25 00:52:35 +0900

    second session

--- sns_project/sns_project/settings.py
+++ sns_project/sns_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*2q8&_gu%ceelyg(+%r^(r58z$k19r5$de&$1r%4ed%#g(0(o@'
+SECRET_KEY = 'django-insecure-^pksdznbqekh)ukvebw)0fv!q$^zn#0d5@x#ntawe8i263-u^0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
